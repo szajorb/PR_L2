22ec032101de4a90d8d661ddabcf4bac05c9d090
bszaj <b.szajor@gmail.com>
2021-03-19 14:58:00 +0100

    opis zmian

---
+++ js/navbar.js
@@ -0,0 +1,7 @@
+let nav = '<ul><li><a href="index.html">Strona glowna</a></li> '+
+    '<li><a href="kontakt.html">Kontakt</a></li></ul>';
+
+let navbarElem = document.querySelector(".navbar"); //lub inny sposób opisany wy¿ej
+
+navbarElem.innerHTML = nav;
+
